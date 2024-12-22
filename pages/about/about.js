482cbdbfcf4557214c24d3662bdf17c3bfac6443
JohnnyAstrom <johnny.astrom@hotmail.com>
2024-12-22 12:54:09 +0100

    Created basic fetch function.

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,45 +1,62 @@
-createSections();
+async function fetchAboutPageData() {
+  const res = await fetch('../../data/about.json');
+  const data = await res.json();
+  return {
+    headline: data.headline,
+    aboutPage: data.aboutPage,
+  };
+}
 
-function createSections() {
-  //Headline
-  const headline = document.querySelector('.about-header');
+async function updateDOMWithAboutPageData() {
+  const { headline, aboutPage } = await fetchAboutPageData();
+
+  if (aboutPage && headline) {
+    createSections(aboutPage, headline);
+  }
+}
+
+function createSections(aboutData, pageHeadline) {
+  // Headline
+  const headlineElement = document.querySelector('.about-header');
   const mainheading = document.createElement('h1');
-  mainheading.textContent = 'Title of page';
-  headline.appendChild(mainheading);
+  mainheading.textContent = pageHeadline;
+  headlineElement.appendChild(mainheading);
 
-  //Sektion 1
+  // Sektion 1
   const section1 = document.querySelector('.section-1');
   const heading1 = document.createElement('h2');
-  heading1.textContent = 'Title 1';
+  heading1.textContent = aboutData[0].section;
   const content1 = document.createElement('p');
-  content1.textContent = 'Textcontent';
+  content1.textContent = aboutData[0].content;
   section1.appendChild(heading1);
   section1.appendChild(content1);
 
-  //Sektion 2
+  // Sektion 2
   const section2 = document.querySelector('.section-2');
   const heading2 = document.createElement('h2');
-  heading2.textContent = 'Title 2';
+  heading2.textContent = aboutData[1].section;
   const content2 = document.createElement('p');
-  content2.textContent = 'Textcontent';
+  content2.textContent = aboutData[1].content;
   section2.appendChild(heading2);
   section2.appendChild(content2);
 
-  //Sektion 3
+  // Sektion 3
   const section3 = document.querySelector('.section-3');
   const heading3 = document.createElement('h2');
-  heading3.textContent = 'Title 3';
+  heading3.textContent = aboutData[2].section;
   const content3 = document.createElement('p');
-  content3.textContent = 'Textcontent';
+  content3.textContent = aboutData[2].content;
   section3.appendChild(heading3);
   section3.appendChild(content3);
 
-  //Sektion 4
+  // Sektion 4
   const section4 = document.querySelector('.section-4');
   const heading4 = document.createElement('h2');
-  heading4.textContent = 'Title 4';
+  heading4.textContent = aboutData[3].section;
   const content4 = document.createElement('p');
-  content4.textContent = 'Textcontent';
+  content4.textContent = aboutData[3].content;
   section4.appendChild(heading4);
   section4.appendChild(content4);
 }
+
+updateDOMWithAboutPageData();
