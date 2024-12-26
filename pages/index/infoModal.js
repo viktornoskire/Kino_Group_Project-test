0b30fdeb6bf409e2a24098f287a1ed8a9f7d5b83
ViktorNoskire <vikkerinho@gmail.com>
2024-12-26 23:28:10 +0100

    Create modal and render all text in DOM. Also tried implementing in about page, not working yet, will fix

--- pages/index/infoModal.js
+++ pages/index/infoModal.js
@@ -1,4 +1,4 @@
-export async function loadModal() {
+export async function loadJSON() {
   const response = await fetch('data/infoModal.json');
   const data = await response.json();
   return data;
@@ -10,24 +10,82 @@ const open = document.querySelector('.cinema-open');
 const modal = document.querySelector('.info-modal');
 const list = document.querySelector('.info-modal-list');
 
-export async function buildModal() {
-  const data = await loadModal();
-  let sec = data.sections[1].modal;
-  sec.forEach((section) => {
-    if (section.text === undefined) {
-      let q = section.title;
+export async function buildDoc() {
+  const data = await loadJSON();
+  if (title && open) {
+    // Render text to headerSection where the main title is
+    const headSection = data.sections[0];
+    title.innerText = headSection.title;
+    open.innerText = headSection.text;
+  }
+  //
+  const modalInfo = data.sections[1].modal;
+  modalInfo.forEach((section) => {
+    // If there is no text key in section
+    if (section.text == undefined) {
       const question = document.createElement('p');
       question.setAttribute('class', 'modal-title');
-      question.innerText = q;
-      modal.appendChild(question);
+      question.innerText = section.title;
+      // Prepend means to add it to the top of the children line
+      modal.prepend(question);
+      //
     } else {
-      let q = section.title;
+      // Create elements
       const question = document.createElement('p');
+      const answer = document.createElement('p');
+      const open = document.createElement('img');
+      // Set class attribute to elements
       question.setAttribute('class', 'modal-question');
-      question.innerText = q;
+      answer.setAttribute('class', 'modal-answer');
+      open.setAttribute('class', 'modal-open');
+      // Use JSON data to render text to elements
+      question.innerText = section.title;
+      answer.innerText = section.text;
+      answer.style.display = 'none';
+      open.src = data.buttons.openButton;
+      open.alt = 'lorem ipsum';
+      // Append to list
+      list.appendChild(open);
       list.appendChild(question);
+      // Add event listener to img button
+      open.addEventListener('click', () => {
+        open.classList.toggle('open-button-clicked');
+        if (open.className === 'modal-open open-button-clicked') {
+          open.src = data.buttons.closeButton;
+          answer.style.display = '';
+        } else {
+          open.src = data.buttons.openButton;
+          answer.style.display = 'none';
+        }
+      });
+      // If there's a key named 'open'
+      if ('open' in section) {
+        // Go through the 'open'-section
+        section.open.forEach((i) => {
+          const day = document.createElement('p');
+          const date = document.createElement('p');
+          const time = document.createElement('p');
+          const activity = document.createElement('div');
+          activity.setAttribute('class', `open-${i.datum}`);
+          day.setAttribute('class', 'open-times-day');
+          date.setAttribute('class', 'open-times-date');
+          time.setAttribute('class', 'open-times-time');
+          day.innerText = i.dag;
+          date.innerText = i.datum;
+          time.innerText = i.tid;
+          activity.appendChild(day);
+          activity.appendChild(date);
+          activity.appendChild(time);
+          answer.appendChild(activity);
+          list.appendChild(answer);
+        });
+      }
+      // Skip the whole open times elements and text if there is no 'open' key
+      else {
+        list.appendChild(answer);
+      }
     }
   });
 }
 
-buildModal();
+buildDoc();
